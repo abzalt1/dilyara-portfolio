07309ab40ed6ed096202b262787194f96975b5fc
Abzal Tolembi <abzalt1@MacBook-Air-Abzal.local>
2026-02-28 22:03:40 +0500

    fix: remove legacy rewrite rule for /admin to allow Next API Router

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/admin',
-        destination: '/admin/index.html',
-      },
-    ]
-  },
+  // Config options here
 };
 
 export default nextConfig;
